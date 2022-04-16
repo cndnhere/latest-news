75b7948f636047a46fb7db09a7a78fd7703b6969
cndnhere <cndn.here@gmail.com>
2022-04-16 14:49:28 +0530

    DONE

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "192cdf953765400597dad09615f5ffab";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
